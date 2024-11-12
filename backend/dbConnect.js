f0bbd4a6fd03e76c5c0cf72720188b9dae59e714
shivam <shvam12340987@gmail.com>
2024-11-12 20:57:10 +0530

    Replace bcrypt with bcryptjs to avoid binary compatibility issues

--- backend/dbConnect.js
+++ backend/dbConnect.js
@@ -2,7 +2,9 @@ const mongoose = require("mongoose");
 
 async function connectDb() {
   await mongoose
-    .connect("mongodb://localhost:27017/chat-Fussion")
+    .connect(
+      "mongodb+srv://shivam111:shivam111@cluster0.rpeveqo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+    )
     .then(() => {
       console.log("db connection established");
     })
