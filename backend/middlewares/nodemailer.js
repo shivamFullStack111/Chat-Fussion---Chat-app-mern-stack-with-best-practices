c6c0207bf77920244996a1efdddf5db7644d9787
shivam <shvam12340987@gmail.com>
2026-08-29 10:32:51 +0530

    changes in nodemaile transporter config

--- backend/middlewares/nodemailer.js
+++ backend/middlewares/nodemailer.js
@@ -3,6 +3,8 @@ const { config } = require("../config/config");
 
 exports.transporter = nodemailer.createTransport({
   service: "gmail", // Gmail SMTP service use kar rahe hain
+  port: 587,
+  secure: false,
   auth: {
     user: config.MAIL_USER, // Aapka Gmail address
     pass: config.MAIL_PASSWORD, // Generated app password (replace it with this one)
